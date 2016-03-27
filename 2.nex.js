dcd01a34c5c1790fd09c82233a8293b24fe7bef6
Marek Szkodziński <marekstwola@gmail.com>
2016-03-27 18:32:14 +0200

    juz url dziala, teraz sprobuje ten kod sciagnac na server amazona

--- 2.nex.js
+++ 2.nex.js
@@ -1,10 +1,11 @@
 /*wypisuje sumę liczb, trzeba podać dowolną liczbę argumentów -liczb do dodania*/
 
-var arr = process.argv;
+var arr = process.argv;// są to poszczególne elementy wywołania funkcji: $ node 2.nex.js 4  1 23 2342 343 232 2 2
+
 
 var result =0;
 
-for(var i =2; i<arr.length; i++){
+for(var i =2; i<arr.length; i++){  // pomija dwa pierwsze elementy - "node" i "2.nex.js"
 
 result += Number(arr[i]);
 
--- 3.MyFirstIO.js
+++ 3.MyFirstIO.js
@@ -6,11 +6,12 @@ node 3.MyFirstIO.js /home/marek/WebstormProjects/nodeJSTutorial/readFile.txt
 var fs = require('fs');
 var arr = process.argv;
 
-var buffer = fs.readFileSync(arr[2]);
+var buffer = fs.readFileSync(arr[2]); // trzeci argument funkcji ma być ścieżką do pliku tekstowego
 
 var result = buffer.toString();
 
-var res = result.split('\n').length - 1;
+var res = result.split('\n').length - 1; // robi ze stringu tablicę elementów, kolejny element zaczyna się od miejsca w pliku tekstowym nowej linii
+// i zwraca dłogość tej tablicy (czyli ilość wierszy pliku tekstowego)
 
 
 console.log(res/*, require.resolve('fs')*/);
