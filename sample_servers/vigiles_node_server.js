22c0e7c305099d99ec4da76bbc95b1d19738a2b5
Marek Szkodziński <marekstwola@gmail.com>
2016-05-19 15:21:56 +0200

    nic

--- sample_servers/vigiles_node_server.js
+++ sample_servers/vigiles_node_server.js
@@ -47,4 +47,4 @@ http.createServer(function(request, response) {
     });
 }).listen(80);
 
-//TODO: to (i pliki data vigilesa) umieścić na amazonie
+//TODO: do tego dorzucić opcję GET z tym samym plikiem, żeby vigiles brał go w app.js jako url (skopiować z pliku amazon: nodeSamples/test_with_json.js)
